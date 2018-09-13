f8d48ca4626588e20ab34634cf997ca9ca710175
Jennifer Young <jenniferyoung1012@gmail.com>
2018-09-13 12:24:48 -0700

    fixed microblog.py

--- microblog.py
+++ microblog.py
@@ -4,3 +4,8 @@ from app.models import User, Post
 @app.shell_context_processor
 def make_shell_context():
     return {'db': db, 'User':User, 'Post':Post}
+
+if __name__ == '__main__':
+    #app.debug = True
+    #app.run()
+    app.run(debug=True, host='0.0.0.0', port=5009)
